c2ad16ecf6ff4fe63cda4a627216db9204bd5360
AmiHoppus182 <aiami@live.com>
2019-01-08 16:25:17 -0200

    added unity project

---
+++ AR_Project/Assets/Scripts/Registration/RegistrationScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+//using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+
+public class RegistrationScene : MonoBehaviour {
+
+    public Text username;
+
+    public Text birthDay;
+    public Text birthMonth;
+    public Text birthYear;
+
+    public Toggle boy;
+    public Toggle girl;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+    void GetAllInformation()
+    {
+        var name = username.text;
+        PlayerPrefs.SetString("username", name);
+
+        string[] bday = { birthDay.text, birthMonth.text, birthYear.text }; 
+
+        string bd = string.Join("/", bday);
+        PlayerPrefs.SetString("birthday", bd);
+
+    }
+
+    public void FinishedRegistration()
+    {
+        GetAllInformation();
+	}
+
+    void StartGame() 
+    {
+    
+    }
+}
