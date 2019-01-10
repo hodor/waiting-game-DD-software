8c47e2b4d83a66fd5be51a904eb379289e014e76
AmiHoppus182 <aiami@live.com>
2019-01-10 12:48:08 -0200

    begining main game scene logic

---
+++ AR_Project/Assets/Scripts/MainGame/MainGameScene.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AR_Project.DataClasses.MainData;
+using AR_Project.DataClasses.NestedObjects;
+using UnityEngine;
+
+namespace AR_Project.MainGame
+{
+    public class MainGameScene : MonoBehaviour
+    {
+        public GameObject[] respawns;
+        public GameObject prefabReward;
+
+        GameObject currentRespawn;
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            InstantiateTest();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void InstantiateTest()
+        {
+            Experiment firstExperiment = MainData.instanceData.fakeExperiments.experiments[0];
+            var respawn = CheckRespawn(firstExperiment);
+            Debug.Log("Respawn: " + respawn.ToString());
+            Debug.Log("Respawn position: " + respawn.transform.position.ToString());
+            GameObject firstOne = new GameObject();
+            firstOne.transform.position = respawn.transform.position;
+            Debug.Log("Firstone position: " + firstOne.transform.position.ToString());
+            Debug.Log("Firstone timer: " + firstExperiment.secondPrizeTimer.ToString());
+            firstOne = (GameObject)Instantiate(prefabReward);
+            firstOne.GetComponent<Prize>().timer = firstExperiment.secondPrizeTimer;
+            firstOne.GetComponent<Prize>().StartMoving();
+
+        }
+
+        GameObject CheckRespawn(Experiment experiment)
+        {
+            var timerLanes = MainData.instanceData.config.gameSettings.timeLanes;
+            for(int i=0; i < timerLanes.Length; i++) 
+                if (experiment.secondPrizeTimer == timerLanes[i])
+                    return respawns[i];
+
+            return null;
+        }
+
+
+    }
+
+}
---
+++ AR_Project/Assets/Scripts/MainGame/Prize.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AR_Project.MainGame
+{
+    public class Prize : MonoBehaviour {
+    
+        public int timer;
+
+        public void StartMoving()
+        {
+            var finalDestination = new Vector3(gameObject.transform.position.x + 14, gameObject.transform.position.y, 0);
+            StartCoroutine(MoveToPosition(this.gameObject.transform, finalDestination, timer));
+        }
+
+        IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+        {
+            var currentPos = transform.position;
+            var t = 0f;
+            while (t < 1)
+            {
+                t += Time.deltaTime / timeToMove;
+                transform.position = Vector3.Lerp(currentPos, position, t);
+                yield return null;
+            }
+        }
+
+    }
+
+}
