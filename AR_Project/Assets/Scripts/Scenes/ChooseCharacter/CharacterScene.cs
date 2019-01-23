1285ea61c92d7cdb0bc165a1a0c982a8053eb91d
AmiHoppus182 <aiami@live.com>
2019-01-23 16:08:35 -0200

    Choose character scene done

--- AR_Project/Assets/Scripts/Scenes/ChooseCharacter/CharacterScene.cs
+++ AR_Project/Assets/Scripts/Scenes/ChooseCharacter/CharacterScene.cs
@@ -8,40 +8,66 @@ namespace AR_Project.Scenes.ChooseCharacter
 {
     public class CharacterScene : MonoBehaviour
     {
-
-        public List<GameObject> characterList;
-        public GameObject respawnChar;
+        public List<GameObject> characters;    
+        public List<Sprite> characterBigImage;
         public Image charBigImage;
+        public Button leftButton, rightButton;
 
-        int currentIndex;
+        int currentIndex = 0;
 
         // Use this for initialization
         void Start()
         {
-
+            leftButton.enabled = false;
+            UpdateScene(characterBigImage[currentIndex]);
         }
 
-        // Update is called once per frame
-        void Update()
+        void UpdateScene(Sprite bigChar)
         {
-
+            charBigImage.sprite = bigChar;
         }
 
         public void ClickedOnRightButton()
         {
+            if (currentIndex == characterBigImage.Count -1)
+            {
+                rightButton.enabled = false;
+                leftButton.enabled = true;
+            }
+            else
+            {
+                currentIndex++;
+                leftButton.enabled = true;
+                rightButton.enabled = true;
+            } 
 
+            UpdateScene(characterBigImage[currentIndex]);
         }
 
         public void ClickedOnLeftButton()
         {
+            if (currentIndex == 0)
+            {
+                leftButton.enabled = false;
+                rightButton.enabled = true;
+            }
+            else
+            {
+                currentIndex--;
+                leftButton.enabled = true;
+                rightButton.enabled = true;
+            }
 
+
+            UpdateScene(characterBigImage[currentIndex]);
         }
 
 
 
         public void SelectedCharacter()
         {
-
+            PlayerPrefsSaver.instance.character = characters[currentIndex];
+            GoToMainGameScene();
         }
         public void GoToMainGameScene()
         {
