153edf22b945f0d4a3d3085e356e8aac6ba69050
Rogério Gasi <rtgasi@gmail.com>
2019-09-07 20:00:46 -0300

    Calculating subjective value

--- AR_Project/Assets/Scripts/Output/CSV/Calculation/SubjectiveValueData.cs
+++ AR_Project/Assets/Scripts/Output/CSV/Calculation/SubjectiveValueData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -9,7 +10,33 @@ namespace Output.CSV.Calculation
 
         public void Calculate(List<int> points)
         {
+            int num_count = 9;
             values.Add(1000);
+            if (points.Count % num_count != 0)
+            {
+                throw new Exception("Cannot calculate the subjective value data if it's not a multiple of 7");
+            }
+            
+            List<List<int>> grouped = new List<List<int>>();
+            int index = 0;
+            for(int i = 0; i < points.Count; i++)
+            {
+                if (i % num_count == 0 && i != 0) index++;
+                if(grouped.Count <= index) grouped.Add(new List<int>());
+                grouped[index].Add(points[i]);
+            }
+
+            foreach (var group in grouped)
+            {
+                int num_late_choices = 0;
+                foreach (var p in group)
+                {
+                    if (p == 1000) num_late_choices++;
+                }
+
+                values.Add((num_late_choices / group.Count) * 800 + 50);
+            }
+            
         }
         
         public List<string> ToList()
