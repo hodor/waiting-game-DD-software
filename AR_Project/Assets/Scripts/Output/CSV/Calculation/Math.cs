bb4abef5a224b38688457f0e7518dd3210b98a4d
Rogerio Tavares Gasi <rtgasi@gmail.com>
2021-01-27 21:01:37 -0800

    Calculating AUC

--- AR_Project/Assets/Scripts/Output/CSV/Calculation/Math.cs
+++ AR_Project/Assets/Scripts/Output/CSV/Calculation/Math.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AR_Project.DataClasses.MainData;
 
 namespace Output.CSV.Calculation
 {
@@ -6,7 +7,16 @@ namespace Output.CSV.Calculation
     {
         public static float GetAreaUnderCurve(List<int> points)
         {
-            return 0.0f;
+            var timeLanes = MainData.instanceData.config.laneTimes;
+            var time1 = timeLanes[1].time - timeLanes[0].time;
+            var time2 = timeLanes[2].time - timeLanes[1].time;
+            var time3 = timeLanes[3].time - timeLanes[2].time;
+            var time4 = timeLanes[4].time - timeLanes[3].time;
+            var ret = (time1 * ((points[0] + points[1])/2)) + 
+                      (time2 * ((points[1] + points[2])/2)) +
+                      (time3 * ((points[2] + points[3])/2)) +
+                      (time4 * ((points[3] + points[4])/2)) ;
+            return ret;
         }
 
         public static List<float> GetNormalizedValues(SubjectiveValueData values)
